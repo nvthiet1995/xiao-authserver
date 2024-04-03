99caede1785abbf97cd59d2ee160a0ab7c9319d8
tnd <tnd.dev99@gmail.com>
2024-04-03 11:38:32 +0700

    XIAO-109: fix use optional

--- src/main/java/com/xiao/authserver/repository/UserRepository.java
+++ src/main/java/com/xiao/authserver/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.xiao.authserver.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User getUserByEmailAddress(String email);
+    Optional<User> getUserByEmailAddress(String email);
 }
--- src/main/java/com/xiao/authserver/security/XiaoUserDetailsService.java
+++ src/main/java/com/xiao/authserver/security/XiaoUserDetailsService.java
@@ -7,14 +7,16 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Optional;
+
 public class XiaoUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.getUserByEmailAddress(email);
-        if(user != null){
-            return new XiaoUserDetails(user);
+        Optional<User> user = userRepository.getUserByEmailAddress(email);
+        if(user.isPresent()){
+            return new XiaoUserDetails(user.get());
         }
         throw new UsernameNotFoundException("Could not find user with email: "+ email);
     }
