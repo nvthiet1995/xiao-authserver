506f47db6ab6d459d157abb87a9c2b93be747069
tnd <tnd.dev99@gmail.com>
2024-04-04 13:55:01 +0700

    XIAO-109: fix migration files

--- src/main/resources/db/migration/V0_0_1_20240403234120__insert-an-user-into-users-table.sql
+++ src/main/resources/db/migration/V0_0_1_20240403234120__insert-an-user-into-users-table.sql
@@ -2,15 +2,11 @@ INSERT INTO
   `users` (
     `username`,
     `password`,
-    `email_address`,
-    `created_at`,
-    `created_by`
+    `email_address`
   )
 VALUES
   (
     'danh',
     '$2a$12$xj1L/pFygtuvYNqyIlH7BOlY74QdpRSQ.XM.HmrhAKBAr/GXljBJS',
-    'danh@gmail.com',
-    STR_TO_DATE('04-04-2024 09:33:15', '%d-%m-%Y %H:%i:%s'),
-    'flyway'
+    'danh@gmail.com'
   );
--- src/main/resources/db/migration/V0_0_1_20240404133520__remove-redundant-column-on-users-table.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `users` DROP COLUMN `created_by`;
-ALTER TABLE `users` DROP COLUMN `updated_at`;
-ALTER TABLE `users` DROP COLUMN `updated_by`;
