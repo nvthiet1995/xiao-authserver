e29a26e4e082a9756d835994850ff8ba39853999
TND <danh.truong@southtelecom.vn>
2024-05-17 16:58:04 +0700

    XIAO-107: implement authority for user

--- src/main/java/com/xiao/authserver/entity/User.java
+++ src/main/java/com/xiao/authserver/entity/User.java
@@ -1,5 +1,8 @@
 package com.xiao.authserver.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -22,4 +25,11 @@ public class User {
     @Column(name = "email_address")
     private String emailAddress;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 }
